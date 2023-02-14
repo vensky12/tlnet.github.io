f180a88687da1bee9ac55cd861bf7246e6a77a35
vensky12 <45071252+vensky12@users.noreply.github.com>
2023-02-14 22:31:46 +0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -104,7 +104,7 @@ function timeElapse(c){
         f = "0" + f
     }
   //  var a = '<span class="digit">' + g + '</span> 天 <span class="digit">' + b + '</span> 时 <span class="digit">' + d + '</span> 分 <span class="digit">' + f + "</span> 秒";
-  var a = '        相识的第 <span class="digit">' + g + '</span> 天'
+  var a = '          相识的第 <span class="digit">' + g + '</span> 天'
     $("#elapseClock").html(a)
 }
 
